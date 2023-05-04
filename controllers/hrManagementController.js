0628550aac295b777f2a5b306bdeb03474e66a04
Min-Huang <minh.huang96@gmail.com>
2023-05-04 11:04:51 -0700

    finished getAllEmployee route

--- controllers/hrManagementController.js
+++ controllers/hrManagementController.js
@@ -1,6 +1,6 @@
 const userModel = require('../models/User');
 
 exports.getAllEmployees = async (req, res) => {
-    const employees = await userModel.find({ role: "HR" })
-    res.status(200).json({success: true, employees})
+    const employees = await userModel.find({ role: "employee" })
+    res.status(200).json({success: true, length:employees.length,employees})
 }
