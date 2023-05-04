7098fad4ac97bebe8b6c9bb3d75bd6de3f4b79e7
HHX <haohaoxia111@gmail.com>
2023-05-04 16:09:10 +0800

    Created userSignup in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,59 @@
 const userModel = require('../models/User');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
+const uuid = require('uuid');
 require("dotenv").config();
 
+
 const salt = process.env.JWT_SALT;
+
+// User signup
+const userSignup = async (req, res) => {
+    const { username, email, password } = req.body;
+    console.log('1111');
+    if (!validator.isLength(username, { min: 3, max: 30 })) {
+        return res.status(400).json({
+            success: false,
+            message: 'The length of the username is between 3-30 characters',
+        });
+    }
+    if (!validator.isEmail(email)) {
+        return res
+            .status(400)
+            .json({ success: false, message: 'Incorrect email format' });
+    }
+    if (!validator.isLength(password, { min: 3, max: 30 })) {
+        return res.status(400).json({
+            success: false,
+            message: 'The length of the password is between 3-30 characters',
+        });
+    }
+    //check if email or username is unique
+    const existingEmailUser = await userModel.findOne({ email });
+    const existingUsernameUser = await userModel.findOne({ username });
+    if (existingEmailUser) {
+        return res.status(400).json({
+            success: false,
+            message: 'Email address is already in use',
+        });
+    }
+    if (existingUsernameUser) {
+        return res.status(400).json({
+            success: false,
+            message: 'Username is already in use',
+        });
+    }
+
+    const user = new userModel({
+        _id: uuid.v4(),
+        username,
+        email,
+        password
+    });
+    await userModel.register(user);
+    const token = jwt.sign({ user: user }, salt);
+    res.status(200).json({ success: true, data: user, token: token });
+};
 
 // User login
 const userLogin = async (req, res) => {
@@ -42,5 +92,6 @@ const userLogin = async (req, res) => {
 
 
 module.exports = {
+    userSignup,
     userLogin
 };
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,6 +3,7 @@ const Router = express.Router();
 const authController = require('../controllers/authController');
 const auth = require('../middlewares/authorization');
 
+Router.post('/user/signup', authController.userSignup);
 Router.post('/user/login', authController.userLogin);
 
 module.exports = Router;
