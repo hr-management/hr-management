f79dadb85f9c9633f12396fb527a95f0502c580b
HHX <haohaoxia111@gmail.com>
2023-05-05 08:03:48 +0800

    Updated validator and authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,5 @@
 const userModel = require('../models/User');
 const jwt = require('jsonwebtoken');
-const validator = require('validator');
-const uuid = require('uuid');
 require("dotenv").config();
 
 
@@ -10,33 +8,16 @@ const salt = process.env.JWT_SALT;
 // User signup
 const userSignup = async (req, res) => {
     const { username, email, password } = req.body;
-    console.log('1111');
-    if (!validator.isLength(username, { min: 3, max: 30 })) {
-        return res.status(400).json({
-            success: false,
-            message: 'The length of the username is between 3-30 characters',
-        });
-    }
-    if (!validator.isEmail(email)) {
-        return res
-            .status(400)
-            .json({ success: false, message: 'Incorrect email format' });
-    }
-    if (!validator.isLength(password, { min: 3, max: 30 })) {
-        return res.status(400).json({
-            success: false,
-            message: 'The length of the password is between 3-30 characters',
-        });
-    }
     //check if email or username is unique
     const existingEmailUser = await userModel.findOne({ email });
-    const existingUsernameUser = await userModel.findOne({ username });
     if (existingEmailUser) {
         return res.status(400).json({
             success: false,
             message: 'Email address is already in use',
         });
     }
+
+    const existingUsernameUser = await userModel.findOne({ username });
     if (existingUsernameUser) {
         return res.status(400).json({
             success: false,
@@ -45,7 +26,6 @@ const userSignup = async (req, res) => {
     }
 
     const user = new userModel({
-        _id: uuid.v4(),
         username,
         email,
         password
@@ -58,17 +38,6 @@ const userSignup = async (req, res) => {
 // User login
 const userLogin = async (req, res) => {
     const { username, password } = req.body;
-    if (!validator.isLength(username, { min: 3, max: 30 })) {
-        return res
-            .status(400)
-            .json({ success: false, message: 'he length of the username is between 3-30 characters' });
-    }
-    if (!validator.isLength(password, { min: 3, max: 30 })) {
-        return res.status(400).json({
-            success: false,
-            message: 'The length of the password is between 3-30 characters',
-        });
-    }
     const user = await userModel.findOne({ username });
     if (!user) {
         return res
@@ -79,8 +48,7 @@ const userLogin = async (req, res) => {
         () => {
             const token = jwt.sign({ user: user }, salt);
             res.status(200)
-                .json({ success: true, message: 'Login successful', data: token });
-            // res.status(200).redirect('/personal-info');
+                .json({ success: true, message: 'Login successful', data: user, token });
         },
         (err) => {
             return res
