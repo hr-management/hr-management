419cc09e8de3fb93b6aa1876c4cdb047c56160b5
Min-Huang <minh.huang96@gmail.com>
2023-05-05 14:48:35 -0700

    working on validator test without passing all

--- test/hrInvitation.test.js
+++ test/hrInvitation.test.js
@@ -8,13 +8,38 @@ const RegistrationHistoryModel = require("../models/registrationHistory");
 
 describe("HR Invitation", () => {
   after(async () => {
-    
+    await RegistrationHistoryModel.deleteMany();
   });
 
   it("should connect to mongodb", connectMongoose);
-  
-  it("should fail to use exist email to make invitation", () => {
-    
-  })
-  
+  describe("", () => {
+    before(async () => {
+      await RegistrationHistoryModel.deleteMany();
+    });
+
+    it("should success to make invitation", async () => {
+      const res = await request(server).post("/api/employees/invitation").send({
+        name: "user1",
+        email: "user1@gmail.com",
+      });
+      expect(res.statusCode).to.equal(200);
+      expect(res.body.message).to.equal("invited");
+    });
+
+    it("should fail to use exist email to make invitation", async () => {
+      const res = await request(server).post("/api/employees/invitation").send({
+        name: "user1",
+        email: "user1@gmail.com",
+      });
+      expect(res.statusCode).to.equal(400);
+      expect(res.body.message).to.equal("Email address is already in use");
+    });
+    it("should fail without name to make invitation", async () => {
+      const res = await request(server).post("/api/employees/invitation").send({
+        email: "user2@gmail.com",
+      });
+      expect(res.statusCode).to.equal(400);
+      expect(res.body.message).to.equal("Please provide name.");
+    });
+  });
 });
