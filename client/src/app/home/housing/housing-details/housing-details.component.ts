101dca2c2c9a1792128351ffb59c62dde6305a1e
KikiGuoguo <56179585+guoguorani@users.noreply.github.com>
2023-05-09 19:00:29 -0700

    Add HousingDetailsComponent, FacilityReportsComponent and  FacilityReportDetailsComponent

---
+++ client/src/app/home/housing/housing-details/housing-details.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-housing-details',
+  templateUrl: './housing-details.component.html',
+  styleUrls: ['./housing-details.component.css']
+})
+export class HousingDetailsComponent implements OnInit {
+  address!: string;
+  roommates!: Array<{ name: string; phone: string; }>;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    // Make an HTTP GET request to the /api/housing/house-details endpoint
+    this.http.get<{ address: string, roommates: Array<{ name: string, phone: string }> }>('/api/housing/house-details').subscribe((data) => {
+      this.address = data.address;
+      this.roommates = data.roommates;
+    });
+  }
+}
