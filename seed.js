00ee5462285aaabe5f08eb1dab7867888ae73d4e
KikiGuoguo <56179585+guoguorani@users.noreply.github.com>
2023-05-10 00:28:18 -0700

    Add seed file for housing

--- seed.js
+++ seed.js
@@ -1,5 +1,7 @@
 //hardcoded info for HR
 const userModel = require("./models/User");
+const housingModel = require('./models/housing');
+
 const mongoose = require("mongoose");
 const uuid = require("uuid");
 require("dotenv").config();
@@ -79,3 +81,42 @@ db.once("open", async function () {
       console.error("Error registering user:", err);
     });
 })();
+
+// 
+// (async function () {
+//   await housingModel.deleteMany();
+
+//   const newHousing = new housingModel({
+//     address: '123 Elm St',
+//     roommates: [
+//       {
+//         preferredName: 'Jane',
+//         legalFullName: 'Jane Smith',
+//         phoneNumber: '555-555-5555'
+//       },
+//       {
+//         preferredName: 'Bob',
+//         legalFullName: 'Bob Johnson',
+//         phoneNumber: '444-444-4444'
+//       }
+//     ],
+//     reports: [
+//       {
+//         title: 'Leaky roof',
+//         description: 'The roof is leaking in the living room.',
+//         createdBy: new mongoose.Types.ObjectId(),
+//         timestamp: new Date(),
+//         status: 'Open',
+//         comments: [
+//           {
+//             description: 'I will have a contractor look at it.',
+//             createdBy: new mongoose.Types.ObjectId(),
+//             timestamp: new Date()
+//           }
+//         ]
+//       }
+//     ]
+//   });
+
+//   await newHousing.save();
+// })();
