24a716556596771d3682d24e9faa7b39e5383680
KikiGuoguo <56179585+guoguorani@users.noreply.github.com>
2023-05-13 01:40:10 -0700

    add router guard for HouseDetailsComponent

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,9 +10,9 @@ import { HousingManagementComponent } from './home/hr/housing-management/housing
 import { OnboardingApplicationComponent } from './home/employee/onboarding-application/onboarding-application.component';
 import { PersonalInformationComponent } from './home/employee/personal-information/personal-information.component';
 import { EmployeePersonalInfoComponent } from './home/hr/employee-profiles/employee-personal-info/employee-personal-info.component';
-import { FacilityReportDetailsComponent } from './home/housing/facility-report-details/facility-report-details.component';
-import { FacilityReportsComponent } from './home/housing/facility-reports/facility-reports.component';
-import { HousingDetailsComponent } from './home/housing/housing-details/housing-details.component';
+import { FacilityReportDetailsComponent } from './home/employee/housing/facility-report-details/facility-report-details.component';
+import { FacilityReportsComponent } from './home/employee/housing/facility-reports/facility-reports.component';
+import { HousingDetailsComponent } from './home/employee/housing/housing-details/housing-details.component';
 import { HouseDetailsComponent } from './home/hr/housing-management/house-details/house-details.component';
 
 import { AuthGuard } from './services/routeGuard/authRuard';
@@ -26,9 +26,10 @@ const routes: Routes = [
       { path: 'visa-status-management',canActivate: [HRRoleGuard],component: VisaStatusManagementComponent },
       { path: 'hiring-management',canActivate: [HRRoleGuard],component: HiringManagementComponent },
       { path: 'housing-management', canActivate: [HRRoleGuard],component: HousingManagementComponent },
-      { path: 'house-details/:houseId', component: HouseDetailsComponent },
+      { path: 'house-details/:houseId', canActivate: [HRRoleGuard], component: HouseDetailsComponent },
       { path: 'onboarding-application', component: OnboardingApplicationComponent },
       { path: 'personal-information', component: PersonalInformationComponent },
+      { path: 'assigned-house/:houseId', component: HousingDetailsComponent },
       { path: 'facility-reports', component: FacilityReportsComponent },
       { path: 'facility-reports/:reportId', component: FacilityReportDetailsComponent },
     ]
