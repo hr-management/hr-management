5c919892886e47dd875ac645972cb88b68674f59
HHX <haohaoxia111@gmail.com>
2023-05-14 13:33:11 +0800

    Updated side bar

--- client/src/app/home/employee/employee.component.ts
+++ client/src/app/home/employee/employee.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent {
-    sideBarLinks:any[] =['onboarding-application','personal-information']
+    sideBarLinks:any[] =['onboarding-application','personal-information', 'visa-status']
     constructor(private router: Router) { }
 
   ngOnInit() {
